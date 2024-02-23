d2234801a500153236d06e2964dc9d8a2865f8fb
jzgom067 <jzgombic3@gmail.com>
2024-02-23 17:34:33 -0500

    Implement departments SQL conversion script
    Python code was written that takes schools.json from QuACS data, and turns it into SQL that inserts the data into the database in our format.

--- quacs_sql_conversion/schools_to_sql.py
+++ quacs_sql_conversion/schools_to_sql.py
@@ -1,8 +1,20 @@
-"""
-Structure of SQL output:
+import sys
+import json
 
-departments:
-PKEY dept_code
-dept_name
-school_name
-"""
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Not enough arguments")
+        exit(1)
+    elif len(sys.argv) > 3:
+        print("Too many arguments")
+    sql_start = "INSERT INTO departments (dept_code, dept_name, school_name) VALUES\n\t"
+    sql_mid = ""
+    sql_end = ";"
+    with open(sys.argv[1], 'r') as json_file:
+        json_data = json.load(json_file)
+        for school in json_data:
+            for dept in school["depts"]:
+                sql_mid += "('" + dept["code"] + "', '" + dept["name"] + "', '" + school["name"] + "'),\n\t"
+        sql_mid = sql_mid[:-3]
+    with open(sys.argv[2], "w") as sql_file:
+        sql_file.write(sql_start + sql_mid + sql_end)
