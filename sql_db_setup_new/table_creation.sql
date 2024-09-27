d1e6a2e415331b16d7784c05d2569e50f024d841
jzgom067 <jzgombic3@gmail.com>
2024-09-27 16:58:34 -0400

    Add (partial) new database structure
    These are the tables we know are set in stone (mostly).

---
+++ sql_db_setup_new/table_creation.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS courses(
+    dept VARCHAR(4) NOT NULL,
+    code_num SMALLINT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    desc_text TEXT NOT NULL,
+    CONSTRAINT pk_courses PRIMARY KEY(dept, code_num)
+) COMMENT 'Courses from the RPI catalog';
+
+CREATE TABLE IF NOT EXISTS course_availability(
+    dept VARCHAR(4) NOT NULL,
+    code_num SMALLINT NOT NULL,
+    semester SMALLINT NOT NULL,
+    CONSTRAINT pk_course_avail PRIMARY KEY(dept, code_num, semester),
+    CONSTRAINT fk_course_avail_courses
+        FOREIGN KEY(dept, code_num) REFERENCES courses(dept, code_num)
+) COMMENT 'Courses from the RPI catalog';
+
+CREATE TABLE IF NOT EXISTS professors(
+    semester SMALLINT NOT NULL,
+    dept VARCHAR(4) NOT NULL,
+    code_num SMALLINT NOT NULL,
+    prof_name VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_professors PRIMARY KEY(semester, dept, code_num, prof_name),
+    CONSTRAINT fk_professors_courses
+        FOREIGN KEY(dept, code_num) REFERENCES courses(dept, code_num)
+) COMMENT 'Professors for each course';
+
+CREATE TABLE IF NOT EXISTS seats(
+    semester SMALLINT NOT NULL,
+    dept VARCHAR(4) NOT NULL,
+    code_num SMALLINT NOT NULL,
+    seats_filled SMALLINT NOT NULL,
+    seats_total SMALLINT NOT NULL,
+    CONSTRAINT pk_seats PRIMARY KEY(semester, dept, code_num),
+    CONSTRAINT fk_seats_courses
+        FOREIGN KEY(dept, code_num) REFERENCES courses(dept, code_num)
+) COMMENT 'Seats filled in each course';
+
+CREATE TABLE IF NOT EXISTS credits(
+    semester SMALLINT NOT NULL,
+    dept VARCHAR(4) NOT NULL,
+    code_num SMALLINT NOT NULL,
+    credits TINYINT NOT NULL,
+    CONSTRAINT pk_credits PRIMARY KEY(semester, dept, code_num, credits),
+    CONSTRAINT fk_credits_courses
+        FOREIGN KEY(dept, code_num) REFERENCES courses(dept, code_num)
+) COMMENT 'Offered credits for each course';
