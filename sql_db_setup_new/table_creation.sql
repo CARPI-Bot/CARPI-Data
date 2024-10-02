cf942d69646430a449a74a9b46eb78220a4f9c9d
jzgom067 <jzgombic3@gmail.com>
2024-10-02 00:26:26 -0400

    Change table names to singular
    Not only did I read some random article online saying to do this, but in the long run it will make the queries read more naturally.

--- sql_db_setup_new/table_creation.sql
+++ sql_db_setup_new/table_creation.sql
@@ -1,22 +1,22 @@
 -- drop existing tables to be replaced
-DROP TABLE IF EXISTS seats;
-DROP TABLE IF EXISTS professors;
-DROP TABLE IF EXISTS course_restrictions;
-DROP TABLE IF EXISTS prereq_courses;
-DROP TABLE IF EXISTS prereq_structure;
-DROP TABLE IF EXISTS course_relationships;
+DROP TABLE IF EXISTS course_seats;
+DROP TABLE IF EXISTS professor;
+DROP TABLE IF EXISTS course_restriction;
+DROP TABLE IF EXISTS prereq_course;
+DROP TABLE IF EXISTS prereq_nesting;
+DROP TABLE IF EXISTS course_relationship;
 DROP TABLE IF EXISTS course_availability;
-DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS course;
 
 -- create the tables
-CREATE TABLE IF NOT EXISTS courses(
+CREATE TABLE IF NOT EXISTS course(
     dept VARCHAR(4) NOT NULL,
     code_num SMALLINT NOT NULL,
     title VARCHAR(255) NOT NULL,
     desc_text TEXT NOT NULL,
     credit_min TINYINT NOT NULL,
     credit_max TINYINT NOT NULL,
-    CONSTRAINT pk_courses
+    CONSTRAINT pk_course
         PRIMARY KEY(dept, code_num)
 ) COMMENT 'Courses from the RPI catalog';
 
@@ -27,58 +27,58 @@ CREATE TABLE IF NOT EXISTS course_availability(
     semester ENUM('Fall', 'Spring', 'Summer') NOT NULL,
     CONSTRAINT pk_course_avail
         PRIMARY KEY(dept, code_num, sem_year, semester),
-    CONSTRAINT fk_course_avail_courses
+    CONSTRAINT fk_course_avail_course
         FOREIGN KEY(dept, code_num)
-        REFERENCES courses(dept, code_num)
+        REFERENCES course(dept, code_num)
 ) COMMENT 'Courses from the RPI catalog';
 
-CREATE TABLE IF NOT EXISTS course_relationships(
+CREATE TABLE IF NOT EXISTS course_relationship(
     dept VARCHAR(4) NOT NULL,
     code_num SMALLINT NOT NULL,
     relationship ENUM('Coreq', 'Cross') NOT NULL,
     rel_dept VARCHAR(4) NOT NULL,
     rel_code_num SMALLINT NOT NULL,
-    CONSTRAINT pk_course_rels
+    CONSTRAINT pk_course_rel
         PRIMARY KEY(dept, code_num, relationship, rel_dept, rel_code_num),
-    CONSTRAINT fk_course_rels_courses
+    CONSTRAINT fk_course_rel_course
         FOREIGN KEY(dept, code_num)
-        REFERENCES courses(dept, code_num)
+        REFERENCES course(dept, code_num)
 ) COMMENT 'Courses that are corequisites or crosslists for other courses';
 
-CREATE TABLE IF NOT EXISTS prereq_structure(
+CREATE TABLE IF NOT EXISTS prereq_nesting(
     dept VARCHAR(4) NOT NULL,
     code_num SMALLINT NOT NULL,
     nesting_id TINYINT NOT NULL,
     relationship ENUM('AND', 'OR'),
     parent_id TINYINT,
-    CONSTRAINT pk_prereq_structure
+    CONSTRAINT pk_prereq_nesting
         PRIMARY KEY(dept, code_num, nesting_id),
-    CONSTRAINT fk_prereq_structure_courses
+    CONSTRAINT fk_prereq_nesting_cours
         FOREIGN KEY(dept, code_num)
-        REFERENCES courses(dept, code_num),
-    CONSTRAINT fk_prereq_structure_parent
+        REFERENCES course(dept, code_num),
+    CONSTRAINT fk_prereq_nesting_parent
         FOREIGN KEY(dept, code_num, parent_id)
-        REFERENCES prereq_structure(dept, code_num, nesting_id)
+        REFERENCES prereq_nesting(dept, code_num, nesting_id)
 ) COMMENT 'The structure of prerequisite nesting for each course';
 
 
-CREATE TABLE IF NOT EXISTS prereq_courses(
+CREATE TABLE IF NOT EXISTS prereq_course(
     dept VARCHAR(4) NOT NULL,
     code_num SMALLINT NOT NULL,
     parent_id TINYINT NOT NULL,
     req_dept VARCHAR(4) NOT NULL,
     req_code_num SMALLINT NOT NULL,
-    CONSTRAINT pk_prereq_courses
+    CONSTRAINT pk_prereq_course
         PRIMARY KEY(dept, code_num, parent_id, req_dept, req_code_num),
-    CONSTRAINT fk_prereq_courses_courses
+    CONSTRAINT fk_prereq_course_course
         FOREIGN KEY(dept, code_num)
-        REFERENCES courses(dept, code_num),
-    CONSTRAINT fk_prereq_courses_structure
+        REFERENCES course(dept, code_num),
+    CONSTRAINT fk_prereq_course_nesting
         FOREIGN KEY(dept, code_num, parent_id)
-        REFERENCES prereq_structure(dept, code_num, nesting_id)
+        REFERENCES prereq_nesting(dept, code_num, nesting_id)
 ) COMMENT 'Prerequisites for other courses, nested according to prereq_structure';
 
-CREATE TABLE IF NOT EXISTS course_restrictions(
+CREATE TABLE IF NOT EXISTS course_restriction(
     dept VARCHAR(4) NOT NULL,
     code_num SMALLINT NOT NULL,
     category ENUM(
@@ -92,36 +92,36 @@ CREATE TABLE IF NOT EXISTS course_restrictions(
     ) NOT NULL,
     restr_rule ENUM('Must be', 'May not be') NOT NULL,
     restriction VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_course_restrictions
+    CONSTRAINT pk_course_restriction
         PRIMARY KEY(dept, code_num, category, restr_rule, restriction),
-    CONSTRAINT fk_course_restrictions_courses
+    CONSTRAINT fk_course_restriction_course
         FOREIGN KEY(dept, code_num)
-        REFERENCES courses(dept, code_num)
+        REFERENCES course(dept, code_num)
 ) COMMENT 'Restrictions for each course';
 
-CREATE TABLE IF NOT EXISTS professors(
+CREATE TABLE IF NOT EXISTS professor(
     sem_year SMALLINT NOT NULL,
     semester ENUM('Fall', 'Spring', 'Summer') NOT NULL,
     dept VARCHAR(4) NOT NULL,
     code_num SMALLINT NOT NULL,
     prof_name VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_professors
+    CONSTRAINT pk_professor
         PRIMARY KEY(sem_year, semester, dept, code_num, prof_name),
-    CONSTRAINT fk_professors_courses
+    CONSTRAINT fk_professor_course
         FOREIGN KEY(dept, code_num)
-        REFERENCES courses(dept, code_num)
+        REFERENCES course(dept, code_num)
 ) COMMENT 'Professors for each course';
 
-CREATE TABLE IF NOT EXISTS seats(
+CREATE TABLE IF NOT EXISTS course_seats(
     sem_year SMALLINT NOT NULL,
     semester ENUM('Fall', 'Spring', 'Summer') NOT NULL,
     dept VARCHAR(4) NOT NULL,
     code_num SMALLINT NOT NULL,
     seats_filled SMALLINT NOT NULL,
     seats_total SMALLINT NOT NULL,
-    CONSTRAINT pk_seats
+    CONSTRAINT pk_course_seats
         PRIMARY KEY(sem_year, semester, dept, code_num),
-    CONSTRAINT fk_seats_courses
+    CONSTRAINT fk_course_seats_course
         FOREIGN KEY(dept, code_num)
-        REFERENCES courses(dept, code_num)
+        REFERENCES course(dept, code_num)
 ) COMMENT 'Seats filled per semester for each course';
