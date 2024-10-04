812c3009c0aade3b7a128b3334407267ad8825b7
chickenwang6 <nwang842@gmail.com>
2024-10-04 17:22:53 -0400

    Working on scraping all the Bachelor Major Requirements

--- catalog_scrapers/degree_scraper.py
+++ catalog_scrapers/degree_scraper.py
@@ -43,11 +43,10 @@ def main():
 
             # get all the degree names
             p_names = portfolios.findAll('p', attrs={'style':'padding-left: 30px'})
-            print(len(p_names))
             for name in p_names:
                 strong = name.find('strong').get_text()
-                print(strong)
                 storage_p.append(strong)
+
             
             # get all the degree links and names of degrees
             degree_links = portfolios.findAll('ul', attrs={'class':'program-list'})
@@ -61,8 +60,52 @@ def main():
                     current_degrees.append(name_link_pair)
                 storage_ul.append(current_degrees)
         
-
+            # print(len(storage_ul))
+            # print(storage_ul[0])
+            # print(storage_p)
+            # return
             # time to visit individual links and find the credit requirements!
+            # use "acalog index % 3" to get the year, fall semester, and spring semester. 
+            # will need to separate carefully to get the right information
+            classes_and_requirements = {}
+            for index_of_degree in range(len(storage_p)):
+                # print(storage_ul[index_of_degree])
+                # bachelors
+                if storage_p[index_of_degree] == "Baccalaureate":
+                    for degree in storage_ul[index_of_degree]:
+                        classes_and_requirements[degree[0]] = []
+                        link = degree[1]
+                        requirements = requests.get(link)
+                        if requirements.status_code == 200:
+                            acalog_core_counter = 0
+                            soup = BeautifulSoup(requirements.text, 'html.parser')
+                            portfolios = soup.find('div', attrs={'class':'custom_leftpad_20'})
+                            # get all acalog-cores
+                            acalog_core = portfolios.findAll('div', attrs={'class':'acalog-core'})[0:13]
+                            for i in range(len(acalog_core)):
+                                # print(acalog_core[i])
+                                if i >= 6:
+                                    # junior year
+                                    if i == 6:
+                                        classes_and_requirements["Junior"] = []
+                                    elif i <= 9:
+                                        x = 5
+                                    
+                                    if i == 10:
+                                        classes_and_requirements["Senior"] = []
+                                    elif i > 10:
+                                        x = 6
+                                else:
+                                    if i % 3 == 0:
+                                        # year
+                                        year = acalog_core[i].find('h2').get_text()
+                                        if year == "First Year":
+                                            classes_and_requirements["Freshman"] = []
+                                        elif year == "Second Year":
+                                            classes_and_requirements["Sophomore"] = []
+                
+                        print(classes_and_requirements)
+
         break
 
 
